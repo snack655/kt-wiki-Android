d2ba9aca7dee3a4c4060a7328dca0bd3d2931303
snack655 <cmjcmj1230@gmail.com>
2022-05-16 16:51:02 +0900

    Add : Project
    2022-05-16

---
+++ app/src/main/java/kr/hs/dgsw/smartschool/ktwiki/MainActivity.kt
@@ -0,0 +1,43 @@
+package kr.hs.dgsw.smartschool.ktwiki
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.hs.dgsw.smartschool.ktwiki.ui.theme.KTwikiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KTwikiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KTwikiTheme {
+        Greeting("Android")
+    }
+}
