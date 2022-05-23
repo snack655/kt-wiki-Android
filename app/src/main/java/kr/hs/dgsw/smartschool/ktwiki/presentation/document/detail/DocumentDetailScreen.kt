4741acd2bbf7309071b39c915cbe7f2591121627
snack655 <cmjcmj1230@gmail.com>
2022-05-23 18:06:26 +0900

    Add : Markdown textview
    2022-05-23

--- app/src/main/java/kr/hs/dgsw/smartschool/ktwiki/presentation/document/detail/DocumentDetailScreen.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/ktwiki/presentation/document/detail/DocumentDetailScreen.kt
@@ -1,37 +1,38 @@
 package kr.hs.dgsw.smartschool.ktwiki.presentation.document.detail
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.google.accompanist.flowlayout.FlowRow
-
+import dev.jeziellago.compose.markdowntext.MarkdownText
 
 @Composable
-fun CoinDetailScreen(
+fun DocumentDetailScreen(
     viewModel: DocumentDetailViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
     Box(modifier = Modifier.fillMaxSize()) {
         state.document?.let { document ->
-            Row(
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(20.dp)
             ) {
-                Text(text = document.title)
-                Text(text = document.text)
+
+                MarkdownText(
+                    modifier = Modifier.padding(16.dp),
+                    markdown = document.title
+                )
+                MarkdownText(
+                    modifier = Modifier.padding(16.dp),
+                    markdown = document.text
+                )
             }
         }
 
